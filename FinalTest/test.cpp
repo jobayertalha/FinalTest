7922d919fc6db7815a5b99e5da5ca2b1d6e481b4
jobayertalha <jobayertalha2020@gmail.com>
2025-04-19 14:36:55 +0600

    without just test cases rest of code is done

--- FinalTest/test.cpp
+++ FinalTest/test.cpp
@@ -31,3 +31,50 @@ public:
     }
 };
 
+//  Concrete Products - Linux
+class LinuxWindow : public Window {
+public:
+    std::string render() const override {
+        return "Rendering Linux Window";
+    }
+};
+
+class LinuxScrollBar : public ScrollBar {
+public:
+    std::string render() const override {
+        return "Rendering Linux ScrollBar";
+    }
+};
+
+//  Abstract Factory 
+class GUIFactory {
+public:
+    virtual std::unique_ptr<Window> createWindow() const = 0;
+    virtual std::unique_ptr<ScrollBar> createScrollBar() const = 0;
+    virtual ~GUIFactory() {}
+};
+
+// Concrete Factories
+class WindowsFactory : public GUIFactory {
+public:
+    std::unique_ptr<Window> createWindow() const override {
+        return std::make_unique<WindowsWindow>();
+    }
+
+    std::unique_ptr<ScrollBar> createScrollBar() const override {
+        return std::make_unique<WindowsScrollBar>();
+    }
+};
+
+class LinuxFactory : public GUIFactory {
+public:
+    std::unique_ptr<Window> createWindow() const override {
+        return std::make_unique<LinuxWindow>();
+    }
+
+    std::unique_ptr<ScrollBar> createScrollBar() const override {
+        return std::make_unique<LinuxScrollBar>();
+    }
+};
+
+
